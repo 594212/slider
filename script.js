d05d6045377f3632767127703c5fc1ada94271ec
sul <su1im69n@gmail.com>
2025-02-20 08:46:37 +0300

    mouse reaction

--- script.js
+++ script.js
@@ -1,50 +1,44 @@
-// let width = children[0].getBoundingClientRect().width;
+let width = children[0].getBoundingClientRect().width;
 
-// window.addEventListener('resize', () => {
-//     width = children[0].getBoundingClientRect().width;
-// });
+window.addEventListener('resize', () => {
+    width = children[0].getBoundingClientRect().width;
+});
 
-// window.addEventListener('load', () => {
-//     container.scrollLeft = (container.scrollWidth - container.clientWidth) / 2;
-// });
+window.addEventListener('load', () => {
+    container.scrollLeft = (container.scrollWidth - container.clientWidth) / 2;
+});
 
-// let offset = 0;
-// let translateX = 0;
-// let pressed = false;
-// container.addEventListener("mousedown", (event) => {
-//     offset = event.offsetX;
-//     pressed = true;
-//     console.log('mousedown');
-// });
+let offset = 0;
+let translateX = 0;
+let pressed = false;
+container.addEventListener("mousedown", (event) => {
+    offset = event.offsetX;
+    pressed = true;
+    console.log('mousedown');
+});
 
-// container.addEventListener("mouseup", () => {
-//     console.log('mouseup');
-//     pressed = false;
-// });
-// container.addEventListener("mousemove", (event) => {
-//     if (!pressed) return;
-//     translateX += event.offsetX - offset;
-//     container.style.transform = `translateX(${translateX}px)`;
-//     let index = Math.floor(- translateX / width);
-//     console.log(index);
-//     if (index > 0) {
-//         translateX = 0;
-//         moveRight();
-//         container.style.transform = `translateX(${translateX}px)`;
-//     } else if (index < 0) {
-//         translateX = 0;
-//         window_left();
-//         container.style.transform = `translateX(${translateX}px)`;
-//     }
+container.addEventListener("mouseup", () => {
+    console.log('mouseup');
+    pressed = false;
 
+    let index = Math.trunc(- translateX / width);
+    if (translateX > 0) {
+        return;
+    }
+    for (let i = 1; i <= index; i++) {
+        moveRight();
+        translateX += width;
+        container.style.transform = `translateX(${translateX}px)`;
 
-//     // console.log(index, translateX, width);
-
-//     // children[index].dataset.index = 8 + index;
-//     // children[index].style.gridColumn = 8 + index;
+    }
+});
+container.addEventListener("mousemove", (event) => {
+    if (!pressed) return;
+    translateX += event.offsetX - offset;
+    container.style.transform = `translateX(${translateX}px)`;
 
 
-// });
+});
 
 let button = document.getElementById('right');
 
